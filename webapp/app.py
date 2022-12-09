306d763b76f8fb44c274ca3047d7bdc6610a66ca
swetac2 <103720160+swetac2@users.noreply.github.com>
2022-12-09 13:13:46 -0500

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -27,14 +27,6 @@ db_conn = connections.Connection(
 )
 output = {}
 table = 'employee';
-
-@app.route("/", methods=['GET', 'POST'])
-def home():
-    return render_template('addemp.html', color=color_codes[COLOR])
-
-@app.route("/about", methods=['GET','POST'])
-def about():
-    return render_template('about.html', color=color_codes[COLOR])
     
 @app.route("/addemp", methods=['POST'])
 def AddEmp():
@@ -50,18 +42,6 @@ def AddEmp():
 
     return render_template('addemp.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set") + "; " + err_message, db_connect_result=db_connect_result, name=socket.gethostname(), color=color, GROUP=GROUP, IMAGE_URL_PATH=IMAGE_URL_PATH, IMAGE_URL_S3=IMAGE_URL_S3)
 
-@app.route("/getemp", methods=['GET', 'POST'])
-def GetEmp():
-    return render_template("getemp.html", color=color_codes[COLOR])
-
-
-@app.route("/fetchdata", methods=['GET','POST'])
-def FetchData():
-    emp_id = request.form['emp_id']
-
-    output = {}
-    select_sql = "SELECT emp_id, first_name, last_name, primary_skill, location from employee where emp_id=%s"
-    cursor = db_conn.cursor()
 
     try:
         cursor.execute(select_sql,(emp_id))
@@ -86,24 +66,6 @@ def FetchData():
 if __name__ == '__main__':
     
     # Check for Command Line Parameters for color
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--color', required=False)
-    args = parser.parse_args()
+   
 
-    if args.color:
-        print("Color from command line argument =" + args.color)
-        COLOR = args.color
-        if COLOR_FROM_ENV:
-            print("A color was set through environment variable -" + COLOR_FROM_ENV + ". However, color from command line argument takes precendence.")
-    elif COLOR_FROM_ENV:
-        print("No Command line argument. Color from environment variable =" + COLOR_FROM_ENV)
-        COLOR = COLOR_FROM_ENV
-    else:
-        print("No command line argument or environment variable. Picking a Random Color =" + COLOR)
-
-    # Check if input color is a supported one
-    if COLOR not in color_codes:
-        print("Color not supported. Received '" + COLOR + "' expected one of " + SUPPORTED_COLORS)
-        exit(1)
-
-    app.run(host='0.0.0.0',port=8080,debug=True)
+    app.run(host='0.0.0.0',port=81,debug=True)
