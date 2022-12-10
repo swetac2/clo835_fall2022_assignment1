7ce3f743d4ea1653e027c445e5793676476a79f7
swetac2 <103720160+swetac2@users.noreply.github.com>
2022-12-10 16:04:16 -0500

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -15,10 +15,11 @@ DATABASE = os.environ.get("DATABASE") or "employees"
 COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
 DBPORT = int(os.environ.get("DBPORT"))
 My_name = os.environ.get('MYNAME')
-if os.path.exists('/clo835/config/image_url'):
- My_file = open("/clo835/config/image_url", "r")
- image_url = My_file.read()
- print("Background image url is ",image_url)
+image_url=os.environ.get('image_url')
+#if os.path.exists('/clo835/config/image_url'):
+ #My_file = open("/clo835/config/image_url", "r")
+ #image_url = My_file.read()
+ #print("Background image url is ",image_url)
 
 # Create a connection to the MySQL database
 db_conn = connections.Connection(
@@ -49,6 +50,6 @@ COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink", "lim
     
 @app.route("/", methods=['GET','POST'])
 def main():
-   return render_template('addemp.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set") )
+   return render_template('addemp.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set") +";"+ image_url=image_url)
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=81)
