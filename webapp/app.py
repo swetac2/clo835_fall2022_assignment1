4f5c0917f71b580e500d2b0460e5f1cc5098a0c2
swetac2 <103720160+swetac2@users.noreply.github.com>
2022-12-10 16:31:03 -0500

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -7,12 +7,11 @@ import socket
 
 
 app = Flask(__name__)
-image_url = " "
 DBHOST = os.environ.get("DBHOST") or "localhost"
 DBUSER = os.environ.get("DBUSER") or "root"
 DBPWD = os.environ.get("DBPWD") or "password"
 DATABASE = os.environ.get("DATABASE") or "employees"
-COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
+#COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
 #DBPORT = int(os.environ.get("DBPORT"))
 My_name = os.environ.get('MYNAME')
 image_url=os.environ.get('image_url')
@@ -22,30 +21,30 @@ image_url=os.environ.get('image_url')
  #print("Background image url is ",image_url)
 
 # Create a connection to the MySQL database
-db_conn = connections.Connection(
-    host= DBHOST,
-    port=DBPORT,
-    user= DBUSER,
-    password= DBPWD, 
-    db= DATABASE
+###db_conn = connections.Connection(
+    #host= DBHOST,
+    #port=DBPORT,
+    #user= DBUSER,
+    #password= DBPWD, 
+    #db= DATABASE
     
-)
+    #)
 output = {}
 table = 'employee';
 
 # Define the supported color codes
-color_codes = {
-    "red": "#e74c3c",
-    "green": "#16a085",
-    "blue": "#89CFF0",
-    "blue2": "#30336b",
-    "pink": "#f4c2c2",
-    "darkblue": "#130f40",
-    "lime": "#C1FF9C",
-}
+#color_codes = {
+#    "red": "#e74c3c",
+#    "green": "#16a085",
+#    "blue": "#89CFF0",
+#    "blue2": "#30336b",
+#    "pink": "#f4c2c2",
+#    "darkblue": "#130f40",
+#    "lime": "#C1FF9C",
+#}
 
 # Generate a random color
-COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink", "lime"])
+#COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink", "lime"])
 
     
 @app.route("/", methods=['GET','POST'])
