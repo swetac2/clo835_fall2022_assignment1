e84dff9ec62f101b3fbda14c4bd077d89c20f39d
carolinanonato <csnonato@gmail.com>
2023-02-02 15:59:34 +0000

    git forgot my modules folder

---
+++ terraform/modules/globalvars/output.tf
@@ -0,0 +1,12 @@
+output "default_tags" {
+  value = {
+    "Owner" = "Carolina"
+    "App"   = "WebApp"
+    "Project" = "CLO835"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "A1"
+}
